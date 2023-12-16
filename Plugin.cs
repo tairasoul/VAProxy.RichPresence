fc8643b4e626f5a607f814cb73ee63a00add1a92
tairasoul <100938045+tairasoul@users.noreply.github.com>
2023-12-16 06:27:20 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace DiscordRichPresence
+{
+    [BepInPlugin("tairasoul.vaproxy.richpresence", "RichPresence", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            Log = Logger;
+            Log.LogInfo("RichPresence awake.");
+        }
+
+        private void OnDestroy()
+        {
+            GameObject Presence = new GameObject("RichPresence");
+            DontDestroyOnLoad(Presence);
+            Presence.AddComponent<Presence>();
+            Log.LogInfo("RichPresence setup.");
+        }
+    }
+}
